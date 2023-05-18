63eb9b0e41370f19245ebd31d34ee0bca54fc78e
aitorcas23 <aitorcas23@gmail.com>
2023-05-18 22:00:18 +0200

    Login with cockroach

--- src/db/cockroachdb/select.js
+++ src/db/cockroachdb/select.js
@@ -40,3 +40,22 @@ export const userExists = async (username) => {
         })
     })
 }
+
+export const correctLogin = async (username, password) => {
+    const client = new Client(process.env.DATABASE_URL)
+    return new Promise((resolve, reject) => {
+        client.connect().then((_) => {
+            client.query(`select * from public."User" where username = '${username}' and "password" = '${password}'`, (err, res) => {
+                client.end()
+                if (err) {
+                    console.log(err)
+                    reject(err)
+                } else if(res.rows){
+                    resolve(true)
+                }else{
+                    resolve(false)
+                }
+            })
+        })
+    })
+}
