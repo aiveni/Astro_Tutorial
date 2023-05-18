1edec267258614db4fcdda498cf2196bb34d0bce
aitorcas23 <aitorcas23@gmail.com>
2023-05-18 23:25:10 +0200

    el otro dotevn

--- src/db/cockroachdb/insert.js
+++ src/db/cockroachdb/insert.js
@@ -1,8 +1,6 @@
 import { Client } from "pg"
-import process from 'node:process';
-if (process.env.NODE_ENV !== 'production') {
-    import("dotenv").then((dotenv) => dotenv.config())
-}
+console.log("B")
+console.log(process.env.DATABASE_URL)
 
 export const createUser = (username, password, email) => {
     const client = new Client(process.env.DATABASE_URL)
